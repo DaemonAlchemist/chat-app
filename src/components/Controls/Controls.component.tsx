0fb4253f37fef85049015d5fa34f92731efd807a
Andrew Wittrock <andy@wittrock.us>
2024-04-27 14:55:13 -0500

    Add save/load buttons

--- src/components/Controls/Controls.component.tsx
+++ src/components/Controls/Controls.component.tsx
@@ -1,13 +1,25 @@
-import { Collapse, Switch } from "antd";
+import { Button, Collapse, Switch } from "antd";
 import {ControlsProps} from "./Controls.d";
 import styles from './Controls.module.scss';
 import { useInlineControls, useNotificationsPanel } from "../../lib/state";
+import { FolderOpenOutlined, SaveOutlined } from "@ant-design/icons";
+import { loadData, saveData } from "../../lib/save";
 
 export const ControlsComponent = ({}:ControlsProps) => {
     const [notificationsOpen, setNotificationsOpen] = useNotificationsPanel();
     const [inlineControls, setInlineControls] = useInlineControls();
 
+    const save = () => {saveData("Phone Data");}
+
+    const load = () => {
+        loadData().then(() => window.location.reload());
+    }
+
     return <>
+        <div>
+            <Button type="link" onClick={save}><SaveOutlined /> Save</Button>
+            <Button type="link" onClick={load}><FolderOpenOutlined /> Load</Button>
+        </div>
         <Collapse>
             <Collapse.Panel header="Controls" key="controls">
                 Inline Controls: <Switch
